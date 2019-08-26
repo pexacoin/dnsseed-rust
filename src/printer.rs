5d6231b58ebb98e61bf8df1fb71ec87f8209ae22
Matt Corallo <git@bluematt.me>
2019-08-26 15:57:24 -0400

    Dont flush with a lock held

--- src/printer.rs
+++ src/printer.rs
@@ -44,70 +44,72 @@ impl Printer {
 				let stdout = std::io::stdout();
 				let mut out = stdout.lock();
 
-				let stats = thread_arc.lock().unwrap();
-				if START_SHUTDOWN.load(Ordering::Relaxed) && stats.connection_count == 0 {
-					break;
-				}
+				{
+					let stats = thread_arc.lock().unwrap();
+					if START_SHUTDOWN.load(Ordering::Relaxed) && stats.connection_count == 0 {
+						break;
+					}
 
-				out.write_all(b"\x1b[2J\x1b[;H\n").expect("stdout broken?");
-				for line in stats.lines.iter() {
-					out.write_all(line.as_bytes()).expect("stdout broken?");
-					out.write_all(b"\n").expect("stdout broken?");
-				}
+					out.write_all(b"\x1b[2J\x1b[;H\n").expect("stdout broken?");
+					for line in stats.lines.iter() {
+						out.write_all(line.as_bytes()).expect("stdout broken?");
+						out.write_all(b"\n").expect("stdout broken?");
+					}
 
-				out.write_all(b"\nNode counts by status:\n").expect("stdout broken?");
-				for i in 0..AddressState::get_count() {
-					out.write_all(format!("{:22}: {}\n", AddressState::from_num(i).unwrap().to_str(),
-							store.get_node_count(AddressState::from_num(i).unwrap())
-							).as_bytes()).expect("stdout broken?");
-				}
+					out.write_all(b"\nNode counts by status:\n").expect("stdout broken?");
+					for i in 0..AddressState::get_count() {
+						out.write_all(format!("{:22}: {}\n", AddressState::from_num(i).unwrap().to_str(),
+								store.get_node_count(AddressState::from_num(i).unwrap())
+								).as_bytes()).expect("stdout broken?");
+					}
 
-				out.write_all(format!(
-						"\nCurrent connections open/in progress: {}\n", stats.connection_count).as_bytes()).expect("stdout broken?");
-				out.write_all(format!(
-						"Connections opened each second: {} (\"c x\" to change to x seconds)\n", store.get_u64(U64Setting::ConnsPerSec)
-						).as_bytes()).expect("stdout broken?");
-				out.write_all(format!(
-						"Current block count: {}\n", stats.header_count).as_bytes()).expect("stdout broken?");
-
-				out.write_all(format!(
-						"Timeout for full run (in seconds): {} (\"t x\" to change to x seconds)\n", store.get_u64(U64Setting::RunTimeout)
-						).as_bytes()).expect("stdout broken?");
-				out.write_all(format!(
-						"Minimum protocol version: {} (\"v x\" to change value to x)\n", store.get_u64(U64Setting::MinProtocolVersion)
-						).as_bytes()).expect("stdout broken?");
-				out.write_all(format!(
-						"Subversion match regex: {} (\"s x\" to change value to x)\n", store.get_regex(RegexSetting::SubverRegex).as_str()
-						).as_bytes()).expect("stdout broken?");
-
-				out.write_all(b"\nRetry times (in seconds):\n").expect("stdout broken?");
-				for i in 0..AddressState::get_count() {
-					let scan_secs = store.get_u64(U64Setting::RescanInterval(AddressState::from_num(i).unwrap()));
 					out.write_all(format!(
-							"{:22} ({:2}): {:5} (ie {} hrs, {} min)\n", AddressState::from_num(i).unwrap().to_str(), i,
-							scan_secs, scan_secs / 60 / 60, (scan_secs / 60) % 60,
+							"\nCurrent connections open/in progress: {}\n", stats.connection_count).as_bytes()).expect("stdout broken?");
+					out.write_all(format!(
+							"Connections opened each second: {} (\"c x\" to change to x seconds)\n", store.get_u64(U64Setting::ConnsPerSec)
 							).as_bytes()).expect("stdout broken?");
+					out.write_all(format!(
+							"Current block count: {}\n", stats.header_count).as_bytes()).expect("stdout broken?");
+
+					out.write_all(format!(
+							"Timeout for full run (in seconds): {} (\"t x\" to change to x seconds)\n", store.get_u64(U64Setting::RunTimeout)
+							).as_bytes()).expect("stdout broken?");
+					out.write_all(format!(
+							"Minimum protocol version: {} (\"v x\" to change value to x)\n", store.get_u64(U64Setting::MinProtocolVersion)
+							).as_bytes()).expect("stdout broken?");
+					out.write_all(format!(
+							"Subversion match regex: {} (\"s x\" to change value to x)\n", store.get_regex(RegexSetting::SubverRegex).as_str()
+							).as_bytes()).expect("stdout broken?");
+
+					out.write_all(b"\nRetry times (in seconds):\n").expect("stdout broken?");
+					for i in 0..AddressState::get_count() {
+						let scan_secs = store.get_u64(U64Setting::RescanInterval(AddressState::from_num(i).unwrap()));
+						out.write_all(format!(
+								"{:22} ({:2}): {:5} (ie {} hrs, {} min)\n", AddressState::from_num(i).unwrap().to_str(), i,
+								scan_secs, scan_secs / 60 / 60, (scan_secs / 60) % 60,
+								).as_bytes()).expect("stdout broken?");
+					}
+
+					out.write_all(format!(
+							"\nBGP Routing Table: {} v4 paths, {} v6 paths\n",
+							stats.v4_table_size, stats.v6_table_size).as_bytes()).expect("stdout broken?");
+
+					out.write_all(b"\nCommands:\n").expect("stdout broken?");
+					out.write_all(b"q: quit\n").expect("stdout broken?");
+					out.write_all(format!(
+							"r x y: Change retry time for status x (int value, see retry times section for name mappings) to y (in seconds)\n"
+							).as_bytes()).expect("stdout broken?");
+					out.write_all(format!(
+							"w x: Change the amount of time a node is considered WAS_GOOD after it fails to x from {} (in seconds)\n",
+							store.get_u64(U64Setting::WasGoodTimeout)
+							).as_bytes()).expect("stdout broken?");
+					out.write_all(b"a x: Scan node x\n").expect("stdout broken?");
+					out.write_all(b"b x: BGP Lookup IP x\n").expect("stdout broken?");
+					out.write_all(b"\x1b[s").expect("stdout broken?"); // Save cursor position and provide a blank line before cursor
+					out.write_all(b"\x1b[;H\x1b[2K").expect("stdout broken?");
+					out.write_all(b"Most recent log:\n").expect("stdout broken?");
+					out.write_all(b"\x1b[u").expect("stdout broken?"); // Restore cursor position and go up one line
 				}
-
-				out.write_all(format!(
-						"\nBGP Routing Table: {} v4 paths, {} v6 paths\n",
-						stats.v4_table_size, stats.v6_table_size).as_bytes()).expect("stdout broken?");
-
-				out.write_all(b"\nCommands:\n").expect("stdout broken?");
-				out.write_all(b"q: quit\n").expect("stdout broken?");
-				out.write_all(format!(
-						"r x y: Change retry time for status x (int value, see retry times section for name mappings) to y (in seconds)\n"
-						).as_bytes()).expect("stdout broken?");
-				out.write_all(format!(
-						"w x: Change the amount of time a node is considered WAS_GOOD after it fails to x from {} (in seconds)\n",
-						store.get_u64(U64Setting::WasGoodTimeout)
-						).as_bytes()).expect("stdout broken?");
-				out.write_all(b"a x: Scan node x\n").expect("stdout broken?");
-				out.write_all(b"b x: BGP Lookup IP x\n").expect("stdout broken?");
-				out.write_all(b"\x1b[s").expect("stdout broken?"); // Save cursor position and provide a blank line before cursor
-				out.write_all(b"\x1b[;H\x1b[2K").expect("stdout broken?");
-				out.write_all(b"Most recent log:\n").expect("stdout broken?");
-				out.write_all(b"\x1b[u").expect("stdout broken?"); // Restore cursor position and go up one line
 
 				out.flush().expect("stdout broken?");
 			}
